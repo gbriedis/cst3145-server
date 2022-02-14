d741be5d55f1e5a35c943dbcc967654da9362f70
gbriedis <46389671+gbriedis@users.noreply.github.com>
2022-02-14 00:28:45 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# MobileApps-CW2
+
+To run it
+1-npm install
+2-npm start
---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var app = express();
+app.use(express.static("public"));
+
+// Routes GET requests to /lessons to the request handler
+app.get("/lessons", function (request, response) {
+  response.send([
+    { topic: "Maths", location: "London", price: 100 },
+    { topic: "Maths", location: "Liverpool", price: 80 },
+    { topic: "Maths", location: "Oxford", price: 90 },
+    { topic: "Maths", location: "Bristol", price: 122 },
+  ]);
+});
+
+// GET requests to /users returns user information
+app.get("/user", function (request, response) {
+  response.send({ email: "user@email.com", password: "mypassword" });
+});
+
+// returning a 404 error when path is not found
+app.use(function (request, response) {
+  response.status(404).send("page not found");
+});
+
+app.listen(3000);
+console.log("Listening on port " + 3000);
