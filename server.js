246ae627dd97596a182852d8e9b2dde0fcb6d65a
gbriedis <46389671+gbriedis@users.noreply.github.com>
2022-02-16 05:42:52 +0000

    user post details

--- server.js
+++ server.js
@@ -8,19 +8,21 @@ const uri = "mongodb+srv://gbriedis:strongpw@cluster0.pr4ee.mongodb.net/myFirstD
 app.use(cors())
 
 
-// get all lessons from MongoDB
+
 let database
 let lessons = []
+// connect to CST3145 database which contains multiple collections (lessons/users/orders placed)
 mongoClient.connect(uri, function(err,client){
     database = client.db('cst3145')
 })
 
+// parameter blueprint
 app.param('collectionName', function(req,res,next,collectionName){
   req.collection = database.collection(collectionName)
   return next()
 })
 
-//gets all lessons
+//get Database data of the appropriate collection name
 app.get('/collection/:collectionName',function(req,res,next){
   req.collection.find({}).toArray(function(err,results){
       if (err){
@@ -30,6 +32,15 @@ app.get('/collection/:collectionName',function(req,res,next){
           res.send(results)
       }
   })
+})
+
+//saves new order to the collection
+app.post('/collection/:collectionName',function(req,res,next){
+    console.log("REQ DETAILS!!!!!!!!!!!!")
+    console.log(req)
+
+
+    app.send("Checked")
 })
 
 // returning a 404 error when path is not found
